3996ff7be606ec071a8d83f71d184d305ec3c3dc
coldino <28345893+coldino@users.noreply.github.com>
2025-10-28 17:30:02 +0000

    Use package version directly
    Removes duplication.

--- src/obelisk/__init__.py
+++ src/obelisk/__init__.py
@@ -1,1 +1,7 @@
-__version__ = '0.1.0'
+from importlib.metadata import PackageNotFoundError, version
+
+
+try:
+    __version__ = version('obelisk-manager')  # must match [project].name in pyproject.toml
+except PackageNotFoundError:
+    __version__ = '0.0.0-dev'  # fallback for local dev
